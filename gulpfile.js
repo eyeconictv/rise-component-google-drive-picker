84222439861aa5d56923e2ec62c084e31f8e08b2
Donna Peplinskie <donna.peplinskie@risevision.com>
2016-04-27 08:11:05 -0400

    Update picker UX

--- gulpfile.js
+++ gulpfile.js
@@ -26,15 +26,6 @@
 
   gulp.task("clean", ["clean-dist", "clean-tmp"]);
 
-  gulp.task("config", function() {
-    var env = process.env.NODE_ENV || "dev";
-    gutil.log("Environment is", env);
-
-    return gulp.src(["./src/config/" + env + ".js"])
-      .pipe(rename("config.js"))
-      .pipe(gulp.dest("./src/config"));
-  });
-
   // Defined method of updating:
   // Semantic
   gulp.task("bump", function(){
@@ -44,9 +35,7 @@
   });
 
   gulp.task("lint", function() {
-    return gulp.src([
-      "src/**/*.js",
-      "test/**/*.js"])
+    return gulp.src("src/**/*.js")
       .pipe(jshint())
       .pipe(jshint.reporter("jshint-stylish"))
       .pipe(jshint.reporter("fail"));
@@ -65,7 +54,6 @@
 
   gulp.task("angular", ["angular:html2js", "lint"], function () {
     return gulp.src([
-      "src/config/config.js",
       "src/angular/dtv-google-drive-picker.js",
       "tmp/ng-templates/*.js",
       "src/angular/svc-gapi.js",
@@ -85,7 +73,7 @@
   });
 
   gulp.task("build", function (cb) {
-    runSequence(["clean", "config"], ["js-uglify"], cb);
+    runSequence(["clean"], ["js-uglify"], cb);
   });
 
   gulp.task("e2e:server", factory.testServer());
@@ -108,17 +96,17 @@
       "components/q/q.js",
       "components/angular/angular.js",
       "components/angular-mocks/angular-mocks.js",
+      "components/angular-translate/angular-translate.js",
+      "components/rv-common-i18n/dist/i18n.js",
+      "node_modules/widget-tester/mocks/i18n-config.js",
       "node_modules/widget-tester/mocks/gapi-picker-mock.js",
-      "src/config/test.js",
       "src/angular/*.js",
       "test/unit/**/*spec.js"
     ]
   }));
 
-  gulp.task("test:metrics", factory.metrics());
-
   gulp.task("test", ["build"], function (cb) {
-    return runSequence("test:unit:ng", "test:e2e:ng", "test:metrics", cb);
+    return runSequence("test:unit:ng", "test:e2e:ng", cb);
   });
 
   gulp.task("default", ["build"]);
